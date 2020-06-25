64c1165050f0da8a134cf3ffd7477331693b4caa
Mekarosi <mekarosi_assa@yahoo.com>
2020-06-26 00:17:26 +0100

    Update components

--- client/src/components/CreateTodo.js
+++ client/src/components/CreateTodo.js
@@ -1,10 +1,175 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CreateTodo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_startdate: '',
+      todo_completeddate: '',
+      todo_completed: false,
+    };
+  }
+
+  onChangeTodoDescription = (e) => {
+    this.setState({
+      todo_description: e.target.value,
+    });
+  };
+
+  onChangeTodoResponsible = (e) => {
+    this.setState({
+      todo_responsible: e.target.value,
+    });
+  };
+
+  onChangeTodoPriority = (e) => {
+    this.setState({
+      todo_priority: e.target.value,
+    });
+  };
+
+  onChangeTodoStartDate = (e) => {
+    this.setState({
+      todo_startdate: e.target.value,
+    });
+  };
+
+  onChangeTodoCompleteDate = (e) => {
+    this.setState({
+      todo_completeddate: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(`Form submitted:`);
+    console.log(`Todo Description: ${this.state.todo_description}`);
+    console.log(`Todo Responsible: ${this.state.todo_responsible}`);
+    console.log(`Todo Priority: ${this.state.todo_priority}`);
+    console.log(`Todo Startdate: ${this.state.todo_startdate}`);
+    console.log(`Todo Completeddate: ${this.state.todo_completeddate}`);
+
+    const newTodo = {
+      todo_description: this.state.todo_description,
+      todo_responsible: this.state.todo_responsible,
+      todo_priority: this.state.todo_priority,
+      todo_startdate: this.state.todo_startdate,
+      todo_completeddate: this.state.todo_completeddate,
+      todo_completed: this.state.todo_completed,
+    };
+
+    axios
+      .post('http://localhost:5090/api/todos/add', newTodo)
+      .then((res) => console.log(res.data));
+
+    this.setState({
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_startdate: '',
+      todo_completeddate: '',
+      todo_completed: false,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <p>Welcome to Create Todo Component!!</p>
+      <div className='container'>
+        <div style={{ marginTop: 10 }}>
+          <h3>Create New Todo</h3>
+          <form onSubmit={this.onSubmit}>
+            <div className='form-group'>
+              <label>Description: </label>
+              <input
+                type='text'
+                className='form-control'
+                value={this.state.todo_description}
+                onChange={this.onChangeTodoDescription}
+              />
+            </div>
+
+            <div className='form-group'>
+              <label>Responsible: </label>
+              <input
+                type='text'
+                className='form-control'
+                value={this.state.todo_responsible}
+                onChange={this.onChangeTodoResponsible}
+              />
+            </div>
+            <div className='form-group'>
+              <label>Start Date: </label>
+              <input
+                type='date'
+                className='form-control'
+                value={this.state.todo_startdate}
+                onChange={this.onChangeTodoStartDate}
+              />
+            </div>
+            <div className='form-group'>
+              <label>Intended Completion Date: </label>
+              <input
+                type='date'
+                className='form-control'
+                value={this.state.todo_completeddate}
+                onChange={this.onChangeTodoCompleteDate}
+              />
+            </div>
+
+            <div className='form-group'>
+              <div className='form-check form-check-inline'>
+                <input
+                  className='form-check-input'
+                  type='radio'
+                  name='priorityOptions'
+                  id='priorityLow'
+                  value='Low'
+                  checked={this.state.todo_priority === 'Low'}
+                  onChange={this.onChangeTodoPriority}
+                />
+                <label className='form-check-label'>Low</label>
+              </div>
+              <div className='form-check form-check-inline'>
+                <input
+                  className='form-check-input'
+                  type='radio'
+                  name='priorityOptions'
+                  id='priorityMedium'
+                  value='Medium'
+                  checked={this.state.todo_priority === 'Medium'}
+                  onChange={this.onChangeTodoPriority}
+                />
+                <label className='form-check-label'>Medium</label>
+              </div>
+              <div className='form-check form-check-inline'>
+                <input
+                  className='form-check-input'
+                  type='radio'
+                  name='priorityOptions'
+                  id='priorityHigh'
+                  value='High'
+                  checked={this.state.todo_priority === 'High'}
+                  onChange={this.onChangeTodoPriority}
+                />
+                <label className='form-check-label'>High</label>
+              </div>
+            </div>
+
+            <div className='form-group'>
+              <input
+                type='submit'
+                value='Create Todo'
+                className='btn btn-primary'
+              />
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,13 +1,78 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Todo = (props) => (
+  <tr>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_description}
+    </td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_responsible}
+    </td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_priority}
+    </td>
+
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_startdate}
+    </td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_completeddate}
+    </td>
+    <td>
+      <Link to={'/edit/' + props.todo._id}>Edit</Link>
+    </td>
+
+    <td>
+      <Link to={'/delete/' + props.todo._id}>Delete</Link>
+    </td>
+  </tr>
+);
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5090/api/todos/')
+      .then((response) => {
+        this.setState({ todos: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  todoList() {
+    return this.state.todos.map((currentTodo, i) => {
+      return <Todo todo={currentTodo} key={i} />;
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>Welcome to Todos List Component!!</p>
+        <h3>Todos List</h3>
+        <table className='table table-striped' style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Responsible</th>
+              <th>Priority</th>
+              <th>Start Date</th>
+              <th>Intended Completion Date</th>
+              <th>Edit Todo</th>
+              <th>Delete Todo</th>
+            </tr>
+          </thead>
+          <tbody>{this.todoList()}</tbody>
+        </table>
       </div>
     );
   }
 }
-
 export default TodoList;
