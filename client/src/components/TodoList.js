e4ec5bf91c49e434c8951b8bc998115afc5c1a7c
Mekarosi <mekarosi_assa@yahoo.com>
2020-06-25 23:46:34 +0100

    Create components

---
+++ client/src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to Todos List Component!!</p>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
